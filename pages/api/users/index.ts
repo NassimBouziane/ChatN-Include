16f8821f067f2e783b9abd07377576ac1924c1c1
S.G <fomnen_s@etna-alternance.net>
2022-12-06 15:39:24 +0100

    [API] user crud

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,27 +1,49 @@
 import { PrismaClient } from '@prisma/client';
 
+const bcrypt = require('bcrypt');
+
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   switch (req.method) {
+    // GET all users
+
     case 'GET': {
-      const QueryResult = await prisma.users.findMany();
-      res.send(QueryResult);
-      break; }
+      const users = await prisma.users.findMany();
+      res.json(users);
+      break;
+    }
+
+    // Create new user
+
     case 'POST': {
-      const QueryResult = await prisma.users.create({
-        data: {
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          role_id: req.body.role_id,
-          group_id: req.body.group_id,
-        },
-      });
-      res.send(QueryResult);
-      break; }
+      const data = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        role_id: req.body.role_id,
+        group_id: req.body.group_id,
+      };
+      try {
+        const hash = await bcrypt.hash(data.password, 10);
+        console.log(hash);
+        const newUser = await prisma.users.create({
+          data: {
+            username: data.username,
+            email: data.email,
+            password: `${hash}`,
+            role_id: data.role_id,
+            group_id: data.group_id,
+          },
+        });
+        res.status(200).json(newUser, { success: true });
+      } catch (error) {
+        console.error('Request error', error);
+        res.status(500).json({ error: 'Request error', success: false });
+      }
+      break;
+    }
     default:
-      res.status(403).send();
       break;
   }
 }
