edf2bed45d6138260a84633b0166e07bd5d00080
Jordan <hoarau_j@etna-alternance.net>
2022-12-06 14:36:00 +0100

    [web]login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
 import '../styles/output.css';
+import LoginButton from '../compenant/loginButton';
+import LoginPopup from '../compenant/loginPopup';
 
 export default function Page() {
-  return <h1 className='text-blue-300'>Hello, Next.js!</h1>;
+  return (
+    <div className='bg-gradient-to-r from-[#a371d0] to-[#ed25ddce] w-full h-screen grid grid-cols-2 gap-1 '>
+      <div className='grid grid-rows-3 w-full h-full'>
+        <div></div>
+        <div className="text-white font-serif">          
+          <p className='text-3xl ml-20 flex justify-center'><strong>CHAT'NCLUDE</strong></p>
+          <p className='ml-20 flex justify-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p className='ml-20 flex justify-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p className='ml-20 flex justify-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p className='ml-20 flex justify-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div className='grid grid-rows-3 grid-cols-1 w-full h-full'>
+      
+      <div></div>
+      <div></div>
+      <div className='flex justify-center'><LoginPopup />
+     </div>
+      </div>
+    </div>
+  );
 }
---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import LoginButton from '../compenant/loginButton';
+import '../styles/output.css';
+import LoginPopup from '../compenant/loginPopup';
+import loginButton from '../compenant/loginButton';
+
+export default function test(){
+    return(
+        <div>
+            <LoginButton/>
+        <LoginPopup/>
+        </div>
+    )
+}
