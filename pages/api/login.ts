66a46eb2795ad728bf95e45eecd4901d94c4e18d
S.G <fomnen_s@etna-alternance.net>
2022-12-07 12:01:58 +0100

    [API]login user

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -5,36 +5,40 @@ dotenv.config();
 
 const prisma = new PrismaClient();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 export default async function handler(req, res) {
-  /* const loginUser: log = {
-    email: req.body.email,
-    password: req.body.password,
-  }; */
-
   switch (req.method) {
+    // Login user
     case 'POST': {
       const QueryResult = await prisma.users.findFirst({
         where: {
           email: req.body.email,
-          password: req.body.password,
         },
       });
       console.log(QueryResult);
       if (QueryResult) {
-        const acces = jwt.sign(
-          {
-            email: req.body.email,
-            password: req.body.password,
-          },
-          process.env.JWT_SIGN_SECRET,
-          {
-            expiresIn: '24h',
-          },
-        );
-        res.json({ accesToken: acces });
+        bcrypt
+          .compare(req.body.password, QueryResult.password)
+          .then((valid) => {
+            if (!valid) {
+              res.status(404).send('error: email or password incorrect oulll');
+            } else {
+              const acces = jwt.sign(
+                {
+                  email: req.body.email,
+                  password: req.body.password,
+                },
+                process.env.JWT_SIGN_SECRET,
+                {
+                  expiresIn: '24h',
+                },
+              );
+              res.json({ accesToken: acces });
+            }
+          });
       } else {
-        res.status(404).send('error: Username or password incorrect');
+        res.status(404).send('error: email or password incorrect');
       }
       break;
     }
