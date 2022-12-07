27e66ec42e2f96a9ff5d57a24a17bd5aedb62ec8
S.G <fomnen_s@etna-alternance.net>
2022-12-07 14:52:50 +0100

    [API]login_user

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -35,6 +35,7 @@ export default async function handler(req, res) {
                 },
               );
               res.json({ accesToken: acces });
+              sessionStorage.setItem('token', acces);
             }
           });
       } else {
