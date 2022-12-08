9d8a5d6ec3a070cc093dc3586557ca6c2718cf52
nassimbouziane <bouzianenassim17102002@gmail.com>
2022-12-08 11:23:32 +0100

    [LOGIN] login linked to the api

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
+import {Login} from '../../interfaces/index'
 
 dotenv.config();
 
@@ -16,10 +17,10 @@ export default async function handler(req, res) {
           email: req.body.email,
         },
       });
-      console.log(QueryResult);
+      const body:Login = JSON.parse(req.body)
       if (QueryResult) {
         bcrypt
-          .compare(req.body.password, QueryResult.password)
+          .compare(body.password, QueryResult.password)
           .then((valid) => {
             if (!valid) {
               res.status(404).send('error: email or password incorrect');
@@ -34,7 +35,7 @@ export default async function handler(req, res) {
                   expiresIn: '24h',
                 },
               );
-              res.json({ accesToken: acces });
+              return res.status(200).json(acces);
             }
           });
       } else {
