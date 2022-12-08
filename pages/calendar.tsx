6b1f8ecb9d9b57756298e16be0f3f31823fba95d
Paul <paul@DESKTOP-GVOCG2A.localdomain>
2022-12-08 11:18:41 +0100

    [GARBAGE] git merge is messing with me

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,6 +1,6 @@
-import React, { Component, Component } from 'react';
+import React, { Component } from 'react';
 import {
-  Calendar, momentLocalizer, Calendar, momentLocalizer,
+  Calendar, momentLocalizer,
 } from 'react-big-calendar';
 import moment from 'moment';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
@@ -8,8 +8,7 @@ import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../styles/output.css';
-// Setup the localizer by providing the moment (or globalize, or Luxon) Object
-// to the correct localizer.
+
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
@@ -56,47 +55,6 @@ export default class App extends Component {
                 localizer={localizer}
                 onEventDrop={this.onEventDrop}
                 onEventResize={this.onEventResize}
-                resizable
-              />
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
-}
-
-// Setup the localizer by providing the moment (or globalize, or Luxon) Object
-// to the correct localizer.
-const localizer = momentLocalizer(moment);
-const DnDCalendar = withDragAndDrop(Calendar);
-
-export default class App extends Component {
-  state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment().add(1, 'days').toDate(),
-        title: 'Some title',
-      },
-    ],
-  };
-
-  render() {
-    return (
-      <>
-        <div className="flex w-full h-screen">
-          <div className="w-40 h-full bg-green-500">sidebar</div>
-          <div className="w-1/5 h-full bg-red-500 grid grid-cols-1">
-            message overview
-          </div>
-          <div className="w-3/4 h-full">
-            <div className="App">
-              <DnDCalendar
-                defaultDate={moment().toDate()}
-                defaultView="month"
-                events={this.state.events}
-                localizer={localizer}
                 resizable
               />
             </div>
