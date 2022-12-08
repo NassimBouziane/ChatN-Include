2240ee4aa02a52cb5ee389852fb6b49cf9e43b6b
abdelqodousse boustani <bousta_a@etna-alternance.net>
2022-12-08 11:05:01 +0100

    [WEB] Side Bar

---
+++ pages/messagerie.tsx
@@ -0,0 +1,12 @@
+import SideBar from '../components/sidebar';
+import SideBarIcon from '../components/sidebar_Icon';
+import '../styles/input.css';
+
+export default function Messagerie() {
+  return (<div > <SideBar/>
+    <h1 className='text-center text-black font-serif text-4xl '>Messagerie</h1>
+    <div> </div>
+
+</div>
+  );
+}
