30cd9401d5339e1ee844c784a8c543230ba6fd86
Jordan <hoarau_j@etna-alternance.net>
2022-12-08 11:02:15 +0100

    [WEB] WIP messagerie

--- pages/messagerie.tsx
+++ pages/messagerie.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import SideBar from '../components/sidebar';
 import SideBarIcon from '../components/sidebar_Icon';
 import '../styles/input.css';
@@ -10,3 +11,11 @@ export default function Messagerie() {
 </div>
   );
 }
+=======
+import Message from "../components/message";
+export default function messagerie(){
+    return(
+        <Message/>
+    )
+}
+>>>>>>> ff65981 ([WEB] WIP messagerie)
