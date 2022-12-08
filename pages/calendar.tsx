d0dba8c49dd763449fb716e8feb3c875a435838d
Paul <paul@DESKTOP-GVOCG2A.localdomain>
2022-12-08 16:03:44 +0100

    [WEB] Calendar now fetch events at loading

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,61 +1,54 @@
+/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import {
-  Calendar, momentLocalizer,
-} from 'react-big-calendar';
-import moment from 'moment';
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
-
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../styles/output.css';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import timeGridPlugin from '@fullcalendar/timegrid';
 
-const localizer = momentLocalizer(moment);
-const DnDCalendar = withDragAndDrop(Calendar);
-
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b><br></br>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
 export default class App extends Component {
-  state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment().add(1, 'days').toDate(),
-        title: 'Some title',
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+  }
 
-  onEventResize = (data) => {
-    const { start, end } = data;
-
-    this.setState((state) => {
-      state.events[0].start = start;
-      state.events[0].end = end;
-      return { events: [...state.events] };
-    });
-  };
-
-  onEventDrop = (data) => {
-    console.log(data);
-  };
+  componentDidMount() {
+    // fetch events here
+    fetch('http://localhost:3000/api/events')
+      .then((response) => response.json())
+      .then((events) => {
+        this.setState({
+          events,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
 
   render() {
     return (
       <>
-        <div className="flex w-full h-screen">
+        <div className="flex w-full h-full">
           <div className="w-40 h-full bg-green-500">sidebar</div>
           <div className="w-1/5 h-full bg-red-500 grid grid-cols-1">
             message overview
           </div>
-          <div className="w-3/4 h-full">
-            <div className="App">
-              <DnDCalendar
-              local = {'fr'}
-                defaultDate={moment().toDate()}
-                defaultView="month"
+          <div className="w-3/4 h-full bg-gray-200">
+            <div className="rounded-lg bg-white shadow-2xl m-2">
+              <FullCalendar
+                plugins={[timeGridPlugin]}
+                nowIndicator={true}
+                initialView="timeGridWeek"
                 events={this.state.events}
-                localizer={localizer}
-                onEventDrop={this.onEventDrop}
-                onEventResize={this.onEventResize}
-                resizable
+                eventContent={renderEventContent}
+
               />
             </div>
           </div>
@@ -64,3 +57,6 @@ export default class App extends Component {
     );
   }
 }
+/** {
+          title: 'Titleeee 1', start: '2022-12-08T10:30:00', end: '2022-12-08T11:30:00', color: 'red',
+        }, */
