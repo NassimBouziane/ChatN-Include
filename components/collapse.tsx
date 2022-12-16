5ae6c4c565521ecc59b2651374b6475c5b301e85
nassimbouziane <bouzianenassim17102002@gmail.com>
2022-12-16 11:27:44 +0100

    [LOGOUT] logout bottun in sidebar

--- components/collapse.tsx
+++ components/collapse.tsx
@@ -1,27 +1,57 @@
 import { useEffect, useState } from 'react';
-import { BiChevronDown } from 'react-icons/bi';
+import { BiChevronDown, BiLogOut } from 'react-icons/bi';
 import '../styles/input.css';
 import DropButton from './dropbutton';
 import { getCookie } from 'typescript-cookie';
+import Avatar from '@mui/material/Avatar';
+import { setCookie } from 'typescript-cookie';
 
 export default function Selector() {
   const [username,setUsername] = useState(null)
+  function stringToColor(string: string) {
+    let hash = 0;
+    let i;
+    if(string){
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+  
+    return color;
+  }
+  }
+  function stringAvatar(name: string) {
+    if(name){
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${Array.from(name)[0].toUpperCase()}`,
+    };
+  }
+  }
+  function logout(){    
+    setCookie('id','',0)
+    setCookie('group','',0)
+    setCookie('token','',0)
+    document.location = 'http://localhost:3000/';
+  }
   useEffect(() => {
     const res = fetch(`http://localhost:3000/api/users/${getCookie('id')}`).then((response) => response.json())
     .then((data) => {setUsername(data.username)})
   }, []);
-  return (<div className='ml-[87%] w-48 h-screen font-medium fixed z-20 '>
-  <button className="bg-white w-full p-1 flex items-center justify-center rounded-full  group"><BiChevronDown size={20}/>
+  return (<div className='ml-[0.5%] w-32 h-fit font-medium flex  z-20 '>
+  <button className="bg-white w-full p-1 flex items-center justify-center rounded-full mt-[440%]  group"><BiChevronDown size={20}/>
   <div className='ml-6 text-[#A371D0]'><DropButton text={username}/></div>
-  <img
-            src="https://cdna.artstation.com/p/assets/images/images/044/872/734/large/rodion-vlasov-fin1-1.jpg?1641375316"
-            className="flex ml-10 object-cover h-8 w-8 rounded-full "
-            alt=""
-          />
-  <ul className='absolute hidden group-focus:block top-[3.5%] w-44 bg-white text-[#A371D0] shadow-md mt-5 ml-0 rounded'>
-    <li className='p-2 text-sm hover:bg-[#f3f3f3] hover:text-[#A371D0] rounded'> <DropButton text='Dark Mode'/></li>
-    <li className='p-2 text-sm hover:bg-[#f3f3f3] hover:text-[#A371D0]'><DropButton text='Setting'/></li>
-    <li className='p-2 text-sm hover:bg-[#f3f3f3] hover:text-[#A371D0]'><DropButton text='Log Out'/></li>
+  <Avatar className='flex ml-4 object-cover h-8 w-8 rounded-full ' {...stringAvatar(username)}></Avatar>
+  <ul className='absolute hidden group-focus:block bottom-[3%] w-42 bg-white text-[#A371D0] shadow-md mt-5 ml-0 rounded'>
+    <li onClick={logout} className='p-2 text-sm hover:bg-[#f3f3f3] hover:text-[#A371D0]'><DropButton text='Log Out'/></li>
   </ul>
   </button>
   </div>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,15 +3,15 @@ import { BiChat } from 'react-icons/bi';
 import { FiMail } from 'react-icons/fi';
 import { IoCalendarNumberOutline } from 'react-icons/io5';
 import SideBarIcon from './sidebar_Icon';
+import  Collapse  from "./collapse"
 
 export default function SideBar() {
   return (
         <div className="w-full
                         flex flex-col
                         bg-[#f3f3f3] text-white border-r-2 z-10">
-
-            <SideBarIcon icon={<FiMail size="28"/>}text='Mail'/>
             <a href="/messagerie"><SideBarIcon icon={<BiChat size="28"/>}text='Chat'/></a>
             <a href="/calendar"><SideBarIcon icon={<IoCalendarNumberOutline size="28"/>}text='Calendar'/></a>
+            <Collapse/>
         </div>);
 }
